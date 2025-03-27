040bf6a98f35d39ea6783f559010388c6945289a
Hegerhog <phamgiabao1682003@gmail.com>
2025-03-27 14:13:02 +0700

    Add_Schedule_Client

---
+++ FAPCLClient/Pages/ScheduleManagement/TeacherDetail.cshtml.cs
@@ -0,0 +1,34 @@
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using FAPCL.DTO;
+namespace FAPCLClient.Pages.ScheduleManagement
+{
+    public class TeacherDetailModel : PageModel
+    {
+        private readonly HttpClient _httpClient;
+
+        public TeacherDetailModel(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public string Id { get; set; }
+        public TeacherDto Teacher { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var response = await _httpClient.GetAsync($"http://localhost:5043/api/teachers/{Id}");
+            if (!response.IsSuccessStatusCode)
+            {
+                return NotFound();
+            }
+
+            Teacher = await response.Content.ReadFromJsonAsync<TeacherDto>();
+            return Page();
+        }
+    }
+}
