43aa61b9f7862172050765aa51d676f456c509c0
Hegerhog <phamgiabao1682003@gmail.com>
2025-03-30 14:59:38 +0700

    Admin chỉnh sửa thời khóa biểu dạy của giảng viên

---
+++ FAPCL/DTO/ClassScheduleDto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FAPCL.DTO
+{
+    public class ClassScheduleDto
+    {
+        public int ClassId { get; set; }
+        public int SlotId { get; set; }
+        public string DayOfWeek { get; set; }
+    }
+}
---
+++ FAPCL/DTO/ScheduleConflictDto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FAPCL.DTO
+{
+    public class ScheduleConflictDto
+    {
+        public int ClassId { get; set; }
+        public string DayOfWeek { get; set; }
+        public int SlotId { get; set; }
+    }
+}
